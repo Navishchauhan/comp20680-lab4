b86b4a784e87bbe5022ab66c022f9b6befd8db25
Navish <navishchauhan7@gmail.com>
2020-06-18 22:15:01 -0400

    finished lab- 4

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -3,12 +3,24 @@ const Product = require('../models/product');
 
 exports.show = async (req, res) => {
 	const product = await product.findById(req.params.id);
+	res.render(`${viewPath}/show`,{
+		pageTitle: product.title,
+		product: product
+	});
 };
 
 exports.new = (req, res) => {
-  res.render(`${viewPath}/new`, {
+  res.redirect(`${viewPath}/new`, {
     pageTitle: 'New Product'
   });
 };
 
-exports.create = async (req, res) => {};
+exports.create = async (req, res) => {
+	try{
+		const product = await Product.create(req.body);
+		res.redirect(`/products/${product.id}`);
+	} catch (err) {
+		res.send(err);
+}
+	
+};
